5988df82f21679245b2d46982a6c1062af94ddd0
guruorgoru <siddharthadhakall3722@gmail.com>
2026-09-21 21:48:53 +0545

    Client now can display and recieve WebSocket messages for the chat messages. Backend implementation needed

---
+++ client/classes/log/log.gd
@@ -0,0 +1,20 @@
+class_name Log
+extends RichTextLabel
+
+func _message(message: String, color: Color = Color.WHITE) -> void:
+	append_text("[color=#%s]%s[/color]/n" % [color.to_html(false), str(message)])
+
+func info(msg: String) -> void:
+	_message(msg, Color.WHITE)
+
+func warning(msg: String) -> void:
+	_message(msg, Color.YELLOW)
+
+func error(msg: String) -> void:
+	_message(msg, Color.ORANGE_RED)
+
+func success(msg: String) -> void:
+	_message(msg, Color.LAWN_GREEN)
+
+func chat(sender_name: String, message: String) -> vooid:
+	_message("[color=#%s]%s:[/color] [i]%s[/i]" % [Color.CORNFLOWER_BLUE.to_html(false), sender_name, message])
--- client/main.gd
+++ client/main.gd
@@ -1,18 +1,32 @@
 extends Node
 
 const packets := preload("res://packets.gd")
+@onready var _log = $Log
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	WsClient.connected_to_server.connect(_on_ws_connected_to_server)
+	WsClient.connection_closed.connect(_on_ws_connection_closed)
+	WsClient.packet_recieved.connect(_on_ws_packet_recieved)
+
+	_log.info("Connecting to server...")
+	WsClient.connect_to_url("ws://127.0.0.1:8080/ws")
+
+func _on_ws_connected_to_server() -> void:
 	var packet := packets.Packet.new()
-	packet.set_sender_id(67)
 	var chat_msg := packet.new_chat()
-	chat_msg.set_msg("Hello dude wassup")
-	print(chat_msg)
+	chat_msg.set_msg("Sup Golang")
 
-	var new_packet := packets.Packet.new()
-	new_packet.from_bytes([8, 69, 18, 15, 10, 13, 72, 101, 108, 108, 111, 44, 32, 119, 111, 114, 108, 100 , 33])
-	print(new_packet.get_chat().get_msg())
+	var err := WsClient.send(packet)
+	if err:
+		_log.error("Error sending packet")
+	else:
+		_log.success("Sent packet")
 
+func _on_ws_connection_closed() -> void:
+	_log.error("Connection closed")
+
+func _on_ws_packet_recieved(packet: packets.Packet) -> void:
+	_log.info("Received packet from the server at: %s" % packet)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
