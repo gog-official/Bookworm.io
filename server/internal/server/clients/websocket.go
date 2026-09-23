f81c31ac5fd4ebb174a39d5e44314ef62116665a
programmer9129 <programmer9129@gmail.com>
2026-09-23 13:07:03 +0530

    log 4 complete

--- server/internal/server/clients/websocket.go
+++ server/internal/server/clients/websocket.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"server/internal/server"
+	"server/internal/server/states"
 	"server/pkg/packets"
 
 	"github.com/gorilla/websocket"
@@ -18,6 +19,7 @@ type WebSocketClient struct {
 	hub      *server.Hub
 	sendChan chan *packets.Packet
 	logger   *log.Logger
+	state    server.ClientStateHandler
 }
 
 func NewWebSocketClient(hub *server.Hub, writer http.ResponseWriter, request *http.Request) (server.ClientInterfacer, error) {
@@ -44,17 +46,12 @@ func (c *WebSocketClient) Id() uint64 {
 
 func (c *WebSocketClient) Initialize(id uint64) {
 	c.id = id
-	c.logger.SetPrefix(fmt.Sprintf("clinet %d", c.id))
-	c.SocketSend(packets.NewId(c.id))
-	c.logger.Printf("Sent ID to client")
+	c.logger.SetPrefix(fmt.Sprintf("Client %d", c.id))
+	c.SetState(&states.Connected{})
 }
 
 func (c *WebSocketClient) ProcessMessage(senderId uint64, message packets.Msg) {
-	if senderId == c.id{
-		c.Broadcast(message)//client massage
-	}else{
-		c.SocketSendAs(message, senderId)
-	}
+	c.state.HandleMessage(senderId, message)
 }
 func (c *WebSocketClient) SocketSend(message packets.Msg) {
 	c.SocketSendAs(message, c.id)
@@ -83,39 +80,39 @@ func (c *WebSocketClient) ReadPump() {
 }
 
 func (c *WebSocketClient) Close(reason string) {
-    c.logger.Printf("Closing client connection because: %s", reason)
+	c.logger.Printf("Closing client connection because: %s", reason)
 
-    c.hub.UnregisterChan <- c
-    c.conn.Close()
-    if _, closed := <-c.sendChan; !closed {
-        close(c.sendChan)
-    }
+	c.hub.UnregisterChan <- c
+	c.conn.Close()
+	if _, closed := <-c.sendChan; !closed {
+		close(c.sendChan)
+	}
 	for {
-        _, data, err := c.conn.ReadMessage()
-        if err != nil {
-            if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
-                c.logger.Printf("error: %v", err)
-            }
-            break
-        }
+		_, data, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				c.logger.Printf("error: %v", err)
+			}
+			break
+		}
 
-        packet := &packets.Packet{}
-        err = proto.Unmarshal(data, packet)
-        if err != nil {
-            c.logger.Printf("error unmarshalling data: %v", err)
-            continue
-        }
+		packet := &packets.Packet{}
+		err = proto.Unmarshal(data, packet)
+		if err != nil {
+			c.logger.Printf("error unmarshalling data: %v", err)
+			continue
+		}
 
-        // To allow the client to lazily not set the sender ID, we'll assume they want to send it as themselves
-        if packet.SenderId == 0 {
-            packet.SenderId = c.id
-        }
-
-        c.ProcessMessage(packet.SenderId, packet.Msg)
-    }
+		// To allow the client to lazily not set the sender ID, we'll assume they want to send it as themselves
+		if packet.SenderId == 0 {
+			packet.SenderId = c.id
+		}
+		c.SetState(nil)
+		c.ProcessMessage(packet.SenderId, packet.Msg)
+	}
 }
-func (c *WebSocketClient) WritePump(){
-	defer func(){
+func (c *WebSocketClient) WritePump() {
+	defer func() {
 		c.logger.Println("Closing write pump")
 		c.Close("Write pump closed")
 	}()
@@ -123,25 +120,44 @@ func (c *WebSocketClient) WritePump(){
 	for packet := range c.sendChan {
 		writer, err := c.conn.NextWriter(websocket.BinaryMessage)
 		if err != nil {
-			c.logger.Printf("error getting writer for %T packet, closing client: %v",packet.Msg,err)
+			c.logger.Printf("error getting writer for %T packet, closing client: %v", packet.Msg, err)
 			return
 		}
 		data, err := proto.Marshal(packet)
 		if err != nil {
-			c.logger.Printf("error ,arshaling %T packet, dropping: %v",packet.Msg,err)
-			continue 
+			c.logger.Printf("error ,arshaling %T packet, dropping: %v", packet.Msg, err)
+			continue
 		}
 		_, writeErr := writer.Write(data)
 		if writeErr != nil {
-			c.logger.Printf("error writing %T packet: %v",packet.Msg,writeErr)
+			c.logger.Printf("error writing %T packet: %v", packet.Msg, writeErr)
 			continue
 		}
 		writer.Write([]byte{'\n'})
 
-		if closeErr := writer.Close(); closeErr != nil{
+		if closeErr := writer.Close(); closeErr != nil {
 			c.logger.Printf("error closing writer, dropping %T packet:%v", packet.Msg, closeErr)
 			continue
 		}
 	}
 
+}
+func (c *WebSocketClient) SetState(state server.ClientStateHandler) {
+	prevStateName := "None"
+	if c.state != nil {
+		prevStateName = c.state.Name()
+		c.state.OnExit()
+	}
+
+	newStateName := "None"
+	if state != nil {
+		newStateName = state.Name()
+	}
+
+	c.logger.Printf("switching form state %s to %s", prevStateName, newStateName)
+	c.state = state
+	if c.state != nil {
+		c.state.SetClient(c)
+		c.state.OnEnter()
+	}
 }
--- server/internal/server/states/connected.go
+++ server/internal/server/states/connected.go
@@ -9,7 +9,7 @@ import (
 
 type Connected struct {
 	client server.ClientInterfacer
-	logger *log.logger
+	logger *log.Logger
 }
 
 func (c *Connected) Name() string {
@@ -18,12 +18,17 @@ func (c *Connected) Name() string {
 func (c *Connected) SetClient(client server.ClientInterfacer) {
 	c.client = client
 	loggingPrefix := fmt.Sprintf("Client %d [%s]: ", client.Id(), c.Name())
-	c.logger = log.new(log.new(log.Writer(), loggingPrefix, log.LstdFlags))
+	c.logger = log.New(log.Writer(), loggingPrefix, log.LstdFlags)	
 }
 func (c *Connected) OnEnter() {
 	c.client.SocketSend(packets.NewId(c.client.Id()))
 }
 func (c *Connected) HandleMessage(senderId uint64, message packets.Msg) {
+	if senderId == c.client.Id(){
+		c.client.Broadcast(message)
+	}else {
+		c.client.SocketSendAs(message, senderId)
+	}
 }
 
 func (c *Connected) OnExit() {
