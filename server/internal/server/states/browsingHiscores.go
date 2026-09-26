76cb05660f0d846b337ac9616698fde249420298
programmer9129 <programmer9129@gmail.com>
2026-09-26 14:01:52 +0530

    complete stage 1 update

--- server/internal/server/states/browsingHiscores.go
+++ server/internal/server/states/browsingHiscores.go
@@ -81,6 +81,29 @@ func (b *BrowsingHiscores) handleSearchHiscore(_ uint64, message *packets.Packet
 	offset := playerRank - limit/2
 	b.sendTopScores(limit, max(0, offset))
 }
+func (b *BrowsingHiscores) sendTopScores(limit int64, offset int64){
+	topScores, err := b.querries.GetTopScores(b.dbCtx, db.GetTopScoresParams{
+		Limit: limit,
+		Offset: offset,
+	})
+	if err != nil{
+		b.logger.Printf("Error getting top %d scores from rank %d: %v", limit, offset+1, err)
+		b.client.SocketSend(packets.NewDenyResponse("Failed to get top scores - please try again later"))
+		return 
+	}
+
+	hiscoreMessages := make([]*packets.HiscoreMessage, 0, limit)
+	for rank, scoreRow := range topScores{
+		hiscoreMessage := &packets.HiscoreMessage{
+			Rank: uint64(rank) + uint64(offset) + 1,
+			Name: scoreRow.Name,
+			Score: uint64(scoreRow.BestScore),
+		}
+		hiscoreMessages = append(hiscoreMessages, hiscoreMessage)
+	}
+	b.client.SocketSend(packets.NewHiscoreBoard(hiscoreMessages))
+}
 func (b *BrowsingHiscores) OnExit(){
+	b.sendTopScores(10, 0)
 
 }
