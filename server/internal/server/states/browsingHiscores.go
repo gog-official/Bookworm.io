380bece1cc6ef93f2415d5859948a25bfa9aa785
programmer9129 <programmer9129@gmail.com>
2026-09-26 14:03:00 +0530

    fix some typo

--- server/internal/server/states/browsingHiscores.go
+++ server/internal/server/states/browsingHiscores.go
@@ -1,4 +1,4 @@
-package states 
+package states
 
 import (
 	"context"
@@ -11,40 +11,40 @@ import (
 )
 
 type BrowsingHiscores struct {
-	client server.ClientInterfacer
-	logger *log.Logger
+	client   server.ClientInterfacer
+	logger   *log.Logger
 	querries *db.Queries
-	dbCtx context.Context
+	dbCtx    context.Context
 }
 
 func (b *BrowsingHiscores) Name() string {
 	return "BrowsingHiscores"
 }
 
-func (b *BrowsingHiscores) SetClient(client server.ClientInterfacer){
-	b.client = client 
+func (b *BrowsingHiscores) SetClient(client server.ClientInterfacer) {
+	b.client = client
 	loggingPrefix := fmt.Sprintf("Client %d [%s]", client.Id(), b.Name())
 	b.logger = log.New(log.Writer(), loggingPrefix, log.LstdFlags)
-	b.querries = client .DbTx().Queries
-	b.dbCtx = client.DbTx().Ctx 
+	b.querries = client.DbTx().Queries
+	b.dbCtx = client.DbTx().Ctx
 }
-func (b *BrowsingHiscores) OnEnter(){
+func (b *BrowsingHiscores) OnEnter() {
 	const limit int64 = 10
 	const offset int64 = 0
 	topScores, err := b.querries.GetTopScores(b.dbCtx, db.GetTopScoresParams{
-		Limit: limit,
+		Limit:  limit,
 		Offset: offset,
 	})
-	if err != nil{
+	if err != nil {
 		b.logger.Printf("Error getting top %d scores from rank %d: %v", limit, offset+1, err)
 		b.client.SocketSend(packets.NewDenyResponse("Failed to get top scores - please try again later"))
 		return
 	}
 	hiscoreMessages := make([]*packets.HiscoreMessage, 0, limit)
-	for rank, scoreRow := range topScores{
+	for rank, scoreRow := range topScores {
 		hiscoreMessage := &packets.HiscoreMessage{
-			Rank: uint64(rank + 1) + uint64(offset) + 1,
-			Name: scoreRow.Name,
+			Rank:  uint64(rank+1) + uint64(offset) + 1,
+			Name:  scoreRow.Name,
 			Score: uint64(scoreRow.BestScore),
 		}
 		hiscoreMessages = append(hiscoreMessages, hiscoreMessage)
@@ -52,28 +52,28 @@ func (b *BrowsingHiscores) OnEnter(){
 	b.client.SocketSend(packets.NewHiscoreBoard(hiscoreMessages))
 }
 
-func (b *BrowsingHiscores) HandleMessage(senderId uint64, message packets.Msg){
-	switch message := message.(type){
+func (b *BrowsingHiscores) HandleMessage(senderId uint64, message packets.Msg) {
+	switch message := message.(type) {
 	case *packets.Packet_FinishedBrowsingHiscores:
-		b.handleFinishedBrowsingHiscores( senderId, message) 
+		b.handleFinishedBrowsingHiscores(senderId, message)
 	case *packets.Packet_SearchHiscore:
 		b.handleSearchHiscore(senderId, message)
 	}
 }
-func (b *BrowsingHiscores) handleFinishedBrowsingHiscores(_ uint64, _ *packets.Packet_FinishedBrowsingHiscores){
+func (b *BrowsingHiscores) handleFinishedBrowsingHiscores(_ uint64, _ *packets.Packet_FinishedBrowsingHiscores) {
 	b.client.SetState(&Connected{})
 }
 
-func (b *BrowsingHiscores) handleSearchHiscore(_ uint64, message *packets.Packet_SearchHiscore){
+func (b *BrowsingHiscores) handleSearchHiscore(_ uint64, message *packets.Packet_SearchHiscore) {
 	player, err := b.querries.GetPlayerByName(b.dbCtx, message.SearchHiscore.Name)
-	if err != nil{
+	if err != nil {
 		b.logger.Printf("error getting player %s: %v", message.SearchHiscore.Name, err)
 		b.client.SocketSend(packets.NewDenyResponse("no player found with that name"))
-		return 
+		return
 	}
 	playerRank, err := b.querries.GetPlayerRank(b.dbCtx, player.ID)
-	if err != nil{
-		b.logger.Printf("Error getting player rank %d: %v", message.SearchHiscore.Name, err)
+	if err != nil {
+		b.logger.Printf("Error getting player rank %s: %v", message.SearchHiscore.Name, err)
 		b.client.SocketSend(packets.NewDenyResponse("Player in unranked"))
 		return
 	}
@@ -81,29 +81,29 @@ func (b *BrowsingHiscores) handleSearchHiscore(_ uint64, message *packets.Packet
 	offset := playerRank - limit/2
 	b.sendTopScores(limit, max(0, offset))
 }
-func (b *BrowsingHiscores) sendTopScores(limit int64, offset int64){
+func (b *BrowsingHiscores) sendTopScores(limit int64, offset int64) {
 	topScores, err := b.querries.GetTopScores(b.dbCtx, db.GetTopScoresParams{
-		Limit: limit,
+		Limit:  limit,
 		Offset: offset,
 	})
-	if err != nil{
+	if err != nil {
 		b.logger.Printf("Error getting top %d scores from rank %d: %v", limit, offset+1, err)
 		b.client.SocketSend(packets.NewDenyResponse("Failed to get top scores - please try again later"))
-		return 
+		return
 	}
 
 	hiscoreMessages := make([]*packets.HiscoreMessage, 0, limit)
-	for rank, scoreRow := range topScores{
+	for rank, scoreRow := range topScores {
 		hiscoreMessage := &packets.HiscoreMessage{
-			Rank: uint64(rank) + uint64(offset) + 1,
-			Name: scoreRow.Name,
+			Rank:  uint64(rank) + uint64(offset) + 1,
+			Name:  scoreRow.Name,
 			Score: uint64(scoreRow.BestScore),
 		}
 		hiscoreMessages = append(hiscoreMessages, hiscoreMessage)
 	}
 	b.client.SocketSend(packets.NewHiscoreBoard(hiscoreMessages))
 }
-func (b *BrowsingHiscores) OnExit(){
+func (b *BrowsingHiscores) OnExit() {
 	b.sendTopScores(10, 0)
 
 }
